b67f58d394b8790e1a8961e2304338f50fc35b1c
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2016-10-29 00:46:13 +0200

    fpext, bcm43438: added firmware of bcm43438 (rpi3) and fixed fpext to get an argument to define the ram start address

--- buildtools/flash_patch_extractor/fpext.c
+++ buildtools/flash_patch_extractor/fpext.c
@@ -12,10 +12,6 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define ROM_SIZE (640*1024)
-#define RAM_SIZE (768*1024)
-#define RAM_START (0x180000)
-
 #define AS_STR2(TEXT) #TEXT
 #define AS_STR(TEXT) AS_STR2(TEXT)
 
@@ -25,6 +21,7 @@ long ram_len = 0;
 char *ram_file_name = AS_STR(RAM_FILE_NAME);
 int fp_config_base = 0;
 int fp_config_end = 0;
+int ram_start = 0x180000;
 
 const char *argp_program_version = "fpext";
 const char *argp_program_bug_address = "<mschulz@seemoo.tu-darmstadt.de>";
@@ -33,6 +30,7 @@ static char doc[] = "fpext -- a program to extract flash patches form a firmware
 
 static struct argp_option options[] = {
 	{"ramfile", 'r', "FILE", 0, "Read ram from FILE"},
+	{"ramstart", 's', "ADDR", 0, "RAM start address"},
 	{"fpconfigbase", 'b', "ADDR", 0, "Use ADDR as base address of the flash patch config block"},
 	{"fpconfigend", 'e', "ADDR", 0, "Use ADDR as end address of the flash patch config block"},
 	{ 0 }
@@ -54,6 +52,10 @@ parse_opt(int key, char *arg, struct argp_state *state)
 		case 'e':
 			fp_config_end = strtol(arg, NULL, 0);
 			break;
+
+		case 's':
+			ram_start = strtol(arg, NULL, 0);
+			break;
 		
 		default:
 			return ARGP_ERR_UNKNOWN;
@@ -63,28 +65,6 @@ parse_opt(int key, char *arg, struct argp_state *state)
 }
 
 static struct argp argp = { options, parse_opt, 0, doc };
-
-unsigned char ethernet_ipv6_udp_header_array[] = {
-  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,   /* ETHERNET: Destination MAC Address */
-  'N', 'E', 'X', 'M', 'O', 'N',         /* ETHERNET: Source MAC Address */
-  0x86, 0xDD,                           /* ETHERNET: Type */
-  0x60, 0x00, 0x00, 0x00,               /* IPv6: Version / Traffic Class / Flow Label */
-  0x00, 0x08,                           /* IPv6: Payload Length */
-  0x88,                                 /* IPv6: Next Header = UDPLite */
-  0x01,                                 /* IPv6: Hop Limit */
-  0xFF, 0x02, 0x00, 0x00,               /* IPv6: Source IP */
-  0x00, 0x00, 0x00, 0x00,               /* IPv6: Source IP */
-  0x00, 0x00, 0x00, 0x00,               /* IPv6: Source IP */
-  0x00, 0x00, 0x00, 0x01,               /* IPv6: Source IP */
-  0xFF, 0x02, 0x00, 0x00,               /* IPv6: Destination IP */
-  0x00, 0x00, 0x00, 0x00,               /* IPv6: Destination IP */
-  0x00, 0x00, 0x00, 0x00,               /* IPv6: Destination IP */
-  0x00, 0x00, 0x00, 0x01,               /* IPv6: Destination IP */
-  0xD6, 0xD8,                           /* UDPLITE: Source Port = 55000 */
-  0xD6, 0xD8,                           /* UDPLITE: Destination Port = 55000 */
-  0x00, 0x08,                           /* UDPLITE: Checksum Coverage */
-  0x52, 0x46,                           /* UDPLITE: Checksum only over UDPLITE header*/
-};
 
 int
 read_file_to_array(char *filename, char **buffer, long *filelen)
@@ -109,9 +89,9 @@ read_file_to_array(char *filename, char **buffer, long *filelen)
 int
 get_words(unsigned int addr, unsigned short *low, unsigned short *high)
 {
-	if (addr > RAM_START && addr < RAM_START + ram_len - 4) {
-		*low = *((unsigned short *) (ram_array + addr - RAM_START));
-		*high = *((unsigned short *) (ram_array + addr - RAM_START + 2));
+	if (addr > ram_start && addr < ram_start + ram_len - 4) {
+		*low = *((unsigned short *) (ram_array + addr - ram_start));
+		*high = *((unsigned short *) (ram_array + addr - ram_start + 2));
 		return 2;
 	}
 
@@ -131,7 +111,7 @@ analyse_ram()
 	darm_t *dd = &d;
 	unsigned short low, high;
 	
-	struct fp_config *fpc = (struct fp_config *) (ram_array + fp_config_base - RAM_START);
+	struct fp_config *fpc = (struct fp_config *) (ram_array + fp_config_base - ram_start);
 
 	darm_init(&d);
 
