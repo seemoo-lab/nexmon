909e2eb96be6e25f8270ce3a3a9b2841973a25f1
Daniel Wegemer <daniel@wegemer.com>
2016-11-02 15:41:29 +0100

    setup_env.sh: use different compiler for rpi3

--- setup_env.sh
+++ setup_env.sh
@@ -2,6 +2,7 @@ export ARCH=arm
 export SUBARCH=arm
 
 export HOSTUNAME=$(uname -s)
+export PLATFORMUNAME=$(uname -m)
 
 export NEXMON_ROOT=$(pwd)
 
@@ -9,12 +10,21 @@ if [ $HOSTUNAME == "Darwin" ]; then
 	export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-osx/bin/arm-none-eabi-
 	export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin-osx/nexmon.so
 	export ZLIBFLATE="openssl zlib"
+else if [ $HOSTUNAME == "Linux" ] && [ $PLATFORMUNAME == "x86_64" ]; then
+    export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
+    export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin/nexmon.so
+    export ZLIBFLATE="zlib-flate -compress"
+else if [ $HOSTUNAME == "Linux" ] && [ $PLATFORMUNAME == "armv7l" ]; then
+    export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-armv7l/bin/arm-none-eabi-
+    export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin-arm/nexmon.so
+    export ZLIBFLATE="zlib-flate -compress"
 else
-	export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
-	export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin/nexmon.so
-	export ZLIBFLATE="zlib-flate -compress"
+    echo "Platform not supported!"
+fi
+fi
 fi
 
 export Q=@
 
 export NEXMON_SETUP_ENV=1
+
