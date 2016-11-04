71ff376e0cbbb57dc798479d0885a91bcd6ef6c4
Daniel Wegemer <daniel@wegemer.com>
2016-11-04 11:03:42 +0000

    added new source files (moved injection to sendframe.c)

---
+++ patches/bcm43438/7_45_41_26/nexmon/src/sendframe.c
@@ -0,0 +1,81 @@
+/***************************************************************************
+ *                                                                         *
+ *          ###########   ###########   ##########    ##########           *
+ *         ############  ############  ############  ############          *
+ *         ##            ##            ##   ##   ##  ##        ##          *
+ *         ##            ##            ##   ##   ##  ##        ##          *
+ *         ###########   ####  ######  ##   ##   ##  ##    ######          *
+ *          ###########  ####  #       ##   ##   ##  ##    #    #          *
+ *                   ##  ##    ######  ##   ##   ##  ##    #    #          *
+ *                   ##  ##    #       ##   ##   ##  ##    #    #          *
+ *         ############  ##### ######  ##   ##   ##  ##### ######          *
+ *         ###########    ###########  ##   ##   ##   ##########           *
+ *                                                                         *
+ *            S E C U R E   M O B I L E   N E T W O R K I N G              *
+ *                                                                         *
+ * Warning:                                                                *
+ *                                                                         *
+ * Our software may damage your hardware and may void your hardware’s      *
+ * warranty! You use our tools at your own risk and responsibility!        *
+ *                                                                         *
+ * License:                                                                *
+ * Copyright (c) 2015 NexMon Team                                          *
+ *                                                                         *
+ * Permission is hereby granted, free of charge, to any person obtaining   *
+ * a copy of this software and associated documentation files (the         *
+ * "Software"), to deal in the Software without restriction, including     *
+ * without limitation the rights to use, copy, modify, merge, publish,     *
+ * distribute copies of the Software, and to permit persons to whom the    *
+ * Software is furnished to do so, subject to the following conditions:    *
+ *                                                                         *
+ * The above copyright notice and this permission notice shall be included *
+ * in all copies or substantial portions of the Software.                  *
+ *                                                                         *
+ * Any use of the Software which results in an academic publication or     *
+ * other publication which includes a bibliography must include a citation *
+ * to the author's publication "M. Schulz, D. Wegemer and M. Hollick.      *
+ * NexMon: A Cookbook for Firmware Modifications on Smartphones to Enable  *
+ * Monitor Mode.".                                                         *
+ *                                                                         *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS *
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF              *
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY    *
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,    *
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE       *
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                  *
+ *                                                                         *
+ **************************************************************************/
+
+#pragma NEXMON targetregion "patch"
+
+#include <firmware_version.h>   // definition of firmware version macros
+#include <debug.h>              // contains macros to access the debug hardware
+#include <wrapper.h>            // wrapper definitions for functions that already exist in the firmware
+#include <structs.h>            // structures that are used by the code in the firmware
+#include <helper.h>             // useful helper functions
+#include <patcher.h>            // macros used to craete patches such as BLPatch, BPatch, ...
+#include <rates.h>              // rates used to build the ratespec for frame injection
+#include <nexioctls.h>          // ioctls added in the nexmon patch
+#include <capabilities.h>       // capabilities included in a nexmon patch
+
+void
+sendframe(struct wlc_info *wlc, struct sk_buff *p, unsigned int fifo, unsigned int rate)
+{
+    int short_preamble = 0;
+    struct wlc_txh_info txh = {0};
+
+    if(wlc->band->hwrs_scb) {
+        if (wlc->hw->up) {
+            wlc_d11hdrs(wlc, p, wlc->band->hwrs_scb, short_preamble, 0, 1, 1, 0, 0, rate);
+            p->scb = wlc->band->hwrs_scb;
+            wlc_get_txh_info(wlc, p, &txh);
+            wlc_txfifo(wlc, 1, p, &txh, 1, 1);
+        } else {
+            printf("ERR: wlc down\n");
+        }
+    } else {
+        printf("ERR: no scb found, discarding packet!\n");
+        pkt_buf_free_skb(wlc->osh, p, 0);
+    }
+}
