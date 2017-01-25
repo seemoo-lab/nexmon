e6501be2a550c278791c5f0f2612111b0d69dfad
Manuel Weber <shinji@ikari.cc>
2017-01-25 03:09:21 -0800

    fixes several rpi3 issues and adds zsh support

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,9 @@
 OLD_PWD=$(pwd)
-cd $(dirname ${BASH_SOURCE[0]})
+if [[ $0 == *"bash"* ]]; then
+  cd $(dirname ${BASH_SOURCE[0]})
+elif [[ $0 == *"zsh"* ]]; then
+  cd ${(%):-%N}
+fi
 
 export ARCH=arm
 export SUBARCH=arm
@@ -10,15 +14,15 @@ export PLATFORMUNAME=$(uname -m)
 
 export NEXMON_ROOT=$(pwd)
 
-if [ $HOSTUNAME == "Darwin" ]; then
+if [ $HOSTUNAME '==' "Darwin" ]; then
     export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-osx/bin/arm-none-eabi-
     export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin-osx/nexmon.so
     export ZLIBFLATE="openssl zlib"
-else if [ $HOSTUNAME == "Linux" ] && [ $PLATFORMUNAME == "x86_64" ]; then
+else if [ $HOSTUNAME '==' "Linux" ] && [ $PLATFORMUNAME '==' "x86_64" ]; then
     export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
     export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin/nexmon.so
     export ZLIBFLATE="zlib-flate -compress"
-else if [ $HOSTUNAME == "Linux" ] && [ $PLATFORMUNAME == "armv7l" ]; then
+else if [ $HOSTUNAME '==' "Linux" ] && [ $PLATFORMUNAME '==' "armv7l" ]; then
     export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-armv7l/bin/arm-none-eabi-
     export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin-arm/nexmon.so
     export ZLIBFLATE="zlib-flate -compress"
