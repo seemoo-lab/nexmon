71b0d3af6b7e7f7dd278a3deda5ab1bae7c8aeb2
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2017-02-08 13:52:33 +0100

    bcm43438: moved include files to include directory.

--- patches/bcm43438/7_45_41_26/nexmon/include/bcm43438.h
+++ patches/bcm43438/7_45_41_26/nexmon/include/bcm43438.h
@@ -36,8 +36,8 @@
 #define BCM43438_H
 
 /* addresses of static structs */
-#define SDIO_INFO_ADDR			((void *) 0x6c5a8)
-#define OSL_INFO_ADDR           ((void *) 0x6b950)
-#define WLC_INFO_ADDR           ((void *) 0x6ad6c)
+//#define SDIO_INFO_ADDR			((void *) 0x6c5a8)
+//#define OSL_INFO_ADDR           ((void *) 0x6b950)
+//#define WLC_INFO_ADDR           ((void *) 0x6ad6c)
 
 #endif /*BCM43438_H*/
