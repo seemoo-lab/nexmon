c427dd0f09d493b635f61f7d9c2390c8b6aa5244
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2017-04-20 14:51:55 +0200

    bcm4358, fpext: extended flash_patch_extractor to apply the extracted flash patches directly to an unpatched rom file and used this capability for the bcm4358 rom.

--- buildtools/flash_patch_extractor/fpext.c
+++ buildtools/flash_patch_extractor/fpext.c
@@ -18,10 +18,16 @@
 char *ram_array = NULL;
 long ram_len = 0;
 
+char *rom_array = NULL;
+long rom_len = 0;
+
 char *ram_file_name = AS_STR(RAM_FILE_NAME);
+char *rom_file_in_name = NULL;
+char *rom_file_out_name = NULL;
 int fp_config_base = 0;
 int fp_config_end = 0;
 int ram_start = 0x180000;
+int rom_start = 0x0;
 
 const char *argp_program_version = "fpext";
 const char *argp_program_bug_address = "<mschulz@seemoo.tu-darmstadt.de>";
@@ -33,6 +39,9 @@ static struct argp_option options[] = {
 	{"ramstart", 's', "ADDR", 0, "RAM start address"},
 	{"fpconfigbase", 'b', "ADDR", 0, "Use ADDR as base address of the flash patch config block"},
 	{"fpconfigend", 'e', "ADDR", 0, "Use ADDR as end address of the flash patch config block"},
+	{"romfilein", 'i', "FILE", 0, "Apply patches to this ROM FILE"},
+	{"romfileout", 'o', "FILE", 0, "Save the patched ROM file as FILE"},
+	{"romstart", 't', "ADDR", 0, "ROM start address"},
 	{ 0 }
 };
 
@@ -56,6 +65,18 @@ parse_opt(int key, char *arg, struct argp_state *state)
 		case 's':
 			ram_start = strtol(arg, NULL, 0);
 			break;
+
+		case 'i':
+			rom_file_in_name = arg;
+			break;
+
+		case 'o':
+			rom_file_out_name = arg;
+			break;
+
+		case 't':
+			rom_start = strtol(arg, NULL, 0);
+			break;
 		
 		default:
 			return ARGP_ERR_UNKNOWN;
@@ -67,11 +88,25 @@ parse_opt(int key, char *arg, struct argp_state *state)
 static struct argp argp = { options, parse_opt, 0, doc };
 
 int
+write_array_to_file(char *filename, char *buffer, long filelen)
+{
+	FILE *fileptr;
+
+	if ((fileptr = fopen(filename, "wb"))) {
+		fwrite(buffer, 1, filelen, fileptr);
+		fclose(fileptr);
+		return 0;
+	}
+
+	return -1;
+}
+
+int
 read_file_to_array(char *filename, char **buffer, long *filelen)
 {
 	FILE *fileptr;
 
-	if((fileptr = fopen(filename, "rb"))) {
+	if ((fileptr = fopen(filename, "rb"))) {
 		fseek(fileptr, 0, SEEK_END);
 		*filelen = ftell(fileptr);
 		rewind(fileptr);
@@ -121,6 +156,10 @@ analyse_ram()
 
 		printf("__attribute__((at(0x%08x, \"flashpatch\")))\n", fpc[i].target_addr);
 		printf("BPatch(flash_patch_%d, 0x%08x);\n\n", i, fpc[i].target_addr + dd->imm + 4);
+
+		if (rom_array != NULL && (fpc[i].target_addr - rom_start) < rom_len) {
+			memcpy(&rom_array[fpc[i].target_addr - rom_start], &ram_array[fpc[i].data_ptr - ram_start], fpc[i].size);
+		}
 	}
 }
 
@@ -129,12 +168,23 @@ main(int argc, char **argv)
 {
 	argp_parse(&argp, argc, argv, 0, 0, 0);
 
-	if(!read_file_to_array(ram_file_name, &ram_array, &ram_len)) {
-		fprintf(stderr, "ERR: ram file empty or unavailable.\n");
+	if (!read_file_to_array(ram_file_name, &ram_array, &ram_len)) {
+		fprintf(stderr, "ERR: RAM file empty or unavailable.\n");
 		exit(EXIT_FAILURE);
 	}
 
+	if (rom_file_in_name != NULL && rom_file_out_name != NULL) {
+		if (!read_file_to_array(rom_file_in_name, &rom_array, &rom_len)) {
+			fprintf(stderr, "ERR: ROM file empty or unavailable.\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
 	analyse_ram();
+
+	if (rom_file_in_name != NULL && rom_file_out_name != NULL) {
+		write_array_to_file(rom_file_out_name, rom_array, rom_len);
+	}
 
 	exit(EXIT_SUCCESS);
 }
