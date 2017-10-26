12c368474fec6d89a8ac4af142918d301860eee9
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2017-10-26 23:44:59 +0000

    removed some files from the merge

--- patches/bcm43430a1/7_45_41_46/nexmon/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-make -C /lib/modules/$(uname -r)/build M=$(pwd)/brcmfmac_kernel49 -j2 V=1
-cp brcmfmac_kernel49/brcmfmac.ko /home/pi/P4wnP1/nexmon/brcmfmac.ko
-chown pi:pi /home/pi/P4wnP1/nexmon/brcmfmac.ko
-#source /home/pi/P4wnP1/boot/init_wifi_nexmon.sh
-#WIFI_activate_nexmon
-#/home/pi/P4wnP1/nexmon/nexutil -m6
