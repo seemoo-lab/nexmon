4ef9f4f43aafa846ccbac8b3154283a5b73f2db9
SJuhl75 <stefanjuhl75@gmail.com>
2021-07-16 06:08:57 +0200

    Added build support for Armbian 20.x, using a 5.10.43-sunxi kernel, see README.sunxi

---
+++ patches/bcm43430a1/7_45_41_46/nexmon/brcmfmac_kernel510/brcmfmac.mod.c
@@ -0,0 +1,45 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+MODULE_INFO(depends, "brcmutil,cfg80211");
+
+MODULE_ALIAS("sdio:c*v02D0dA887*");
+MODULE_ALIAS("sdio:c*v02D0d4324*");
+MODULE_ALIAS("sdio:c*v02D0d4329*");
+MODULE_ALIAS("sdio:c*v02D0d4330*");
+MODULE_ALIAS("sdio:c*v02D0d4334*");
+MODULE_ALIAS("sdio:c*v02D0dA94C*");
+MODULE_ALIAS("sdio:c*v02D0dA94D*");
+MODULE_ALIAS("sdio:c*v02D0dA962*");
+MODULE_ALIAS("sdio:c*v02D0dA9A4*");
+MODULE_ALIAS("sdio:c*v02D0d4335*");
+MODULE_ALIAS("sdio:c*v02D0d4339*");
+MODULE_ALIAS("sdio:c*v02D0dA9A6*");
+MODULE_ALIAS("sdio:c*v02D0d4345*");
+MODULE_ALIAS("sdio:c*v02D0dA9BF*");
+MODULE_ALIAS("sdio:c*v02D0d4354*");
+MODULE_ALIAS("sdio:c*v02D0d4356*");
+MODULE_ALIAS("sdio:c*v02D0d4373*");
+MODULE_ALIAS("sdio:c*v02D0dA804*");
