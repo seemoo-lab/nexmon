96c5d9762ce414abcad56862e92e9943119bfaea
DrSchottky <6048264+DrSchottky@users.noreply.github.com>
2022-05-23 23:21:30 +0200

    Revert changes on types.h, nexutil shouldn't depend on this

--- patches/include/types.h
+++ patches/include/types.h
@@ -11,25 +11,19 @@ typedef signed short int        int16_t;
 typedef unsigned short int      uint16_t;
 typedef signed int              int32_t;
 typedef unsigned int            uint32_t;
-#if __WORDSIZE == 64
-typedef unsigned long int       uint64_t;
-typedef signed long int         int64_t;
-typedef long unsigned int       size_t;
-#else
 typedef unsigned long long      uint64_t;
 typedef long long               int64_t;
-typedef uint32_t                size_t;
-#endif
 typedef int8_t                  int8;
 typedef uint8_t                 uint8;
 typedef int16_t                 int16;
 typedef uint16_t                uint16;
 typedef int32_t                 int32;
 typedef uint32_t                uint32;
-typedef long long               int64;
-typedef unsigned long long      uint64;
+typedef int64_t                 int64;
+typedef uint64_t                uint64;
 typedef unsigned char           uchar_t;
 typedef uint32_t                wchar_t;
+typedef uint32_t                size_t;
 typedef uint32_t                addr_t;
 typedef int32_t                 pid_t;
 typedef uint32_t                uint;
