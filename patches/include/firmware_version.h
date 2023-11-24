c37784b880a0cdf39582a97109a5118cac9c1cf2
Jakob Link <jlink@seemoo.tu-darmstadt.de>
2023-11-24 15:01:44 +0100

    bcm4389c1: adding firmware version 20.82.42 (r985250) STA taken from Samsung Galaxy S22 Plus Build UP1A.231005.007.S906BXXU6DWK4

--- patches/include/firmware_version.h
+++ patches/include/firmware_version.h
@@ -133,6 +133,7 @@
 
 // for CHIP_VER_BCM4389c1
 #define FW_VER_20_101_36_2                 810
+#define FW_VER_20_82_42_r985250_sta        811
 
 // for CHIP_VER_BCM43439a0
 #define FW_VER_7_95_49_2271bb6             910
